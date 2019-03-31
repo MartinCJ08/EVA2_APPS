c98e2081446443dd287a01f22d46d7aa78ac3ed8
MartinCJ08 <martin.carrascoj@gmail.com>
2019-03-31 00:07:23 -0700

    Practica 2 terminada

--- EVA2_P2_ASIGNATURAS/app/src/main/java/com/example/eva2_p2_asignaturas/AsignaturaActivity.java
+++ EVA2_P2_ASIGNATURAS/app/src/main/java/com/example/eva2_p2_asignaturas/AsignaturaActivity.java
@@ -1,13 +1,37 @@
 package com.example.eva2_p2_asignaturas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AsignaturaActivity extends AppCompatActivity {
+
+    Intent inDatos;
+    Bundle bDatos;
+
+    TextView txtVwAsig2,txtVwCred,txtVwNomDoc;
+    ImageView imgVwDoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asignatura);
+
+        inDatos = getIntent();
+        bDatos = inDatos.getExtras();
+
+        txtVwAsig2 = findViewById(R.id.txtVwAsig2);
+        txtVwCred = findViewById(R.id.txtVwCred);
+        txtVwNomDoc = findViewById(R.id.txtVwNomDoc);
+        imgVwDoc = findViewById(R.id.imgVwDoc);
+
+        txtVwAsig2.setText(bDatos.getString("nom"));
+        txtVwCred.setText(bDatos.getString("cred"));
+        txtVwNomDoc.setText(bDatos.getString("nomDoc"));
+        imgVwDoc.setImageResource(bDatos.getInt("imgDoc"));
+
+
     }
 }
--- EVA2_P2_ASIGNATURAS/app/src/main/java/com/example/eva2_p2_asignaturas/MainActivity.java
+++ EVA2_P2_ASIGNATURAS/app/src/main/java/com/example/eva2_p2_asignaturas/MainActivity.java
@@ -12,9 +12,19 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
     ListView myList;
 
     static Asignatura[] aaAsig = {
-            new Asignatura(R.drawable.programacion,"Fundamentos de programación","5","John Travolta",5),
-            new Asignatura(R.drawable.programacion,"Programación orientada a objetos","5","John Travolta",5),
-            new Asignatura(R.drawable.programacion,"Estructura de datos","5","John Travolta",5)
+            new Asignatura(R.drawable.programacion,"Fundamentos de programación","5","Homero Simpson",R.drawable.homer),
+            new Asignatura(R.drawable.programacion,"Programación orientada a objetos","5","John Travolta",R.drawable.travolta),
+            new Asignatura(R.drawable.programacion,"Estructura de datos","5","Rick Sánchez",R.drawable.rick),
+            new Asignatura(R.drawable.programacion,"Tópicos avanzados de programación","5","Rick Sánchez",R.drawable.rick),
+            new Asignatura(R.drawable.database,"Fundamentos de bases de datos","5","Daria",R.drawable.daria),
+            new Asignatura(R.drawable.database,"Taller de bases de datos","4","Homero Simpson",R.drawable.homer),
+            new Asignatura(R.drawable.database,"Adminitración de bases de datos","5","John Travolta",R.drawable.travolta),
+            new Asignatura(R.drawable.network,"Fundamentos de telecomunicaciones","4","Daria",R.drawable.daria),
+            new Asignatura(R.drawable.network,"Redes de computadoras","5","Cerebro",R.drawable.cerebro),
+            new Asignatura(R.drawable.network,"Conmutación y enrutamiento de redes de datos","5","Homero Simpson",R.drawable.homer),
+            new Asignatura(R.drawable.network,"Administración de redes","5","Homero Simpson",R.drawable.homer)
+
+
     };
 
 
@@ -36,7 +46,11 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
         inAsig = new Intent(this,AsignaturaActivity.class);
         Bundle bDatos = new Bundle();
         bDatos.putString("nom",aaAsig[position].getNom());
+        bDatos.putString("cred",aaAsig[position].getCreditos());
+        bDatos.putString("nomDoc",aaAsig[position].getDocente());
+        bDatos.putInt("imgDoc",aaAsig[position].getImgDoc());
         inAsig.putExtras(bDatos);
+
         startActivity(inAsig);
     }
 }
